45aedbe8108542783aa0ba83f17ad3d8cc695ee0
itsachaljain <achaljain785@gmail.com>
2020-10-28 01:40:41 +0530

    Footer Fixed

--- components/ChatboxComponent.js
+++ components/ChatboxComponent.js
@@ -1,76 +1,99 @@
 import React, { Component } from "react";
-import { ScrollView, TextInput, StyleSheet, KeyboardAvoidingView, View } from "react-native";
-import { Card, Icon } from "react-native-elements";
+import {
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  View,
+  Text,
+} from "react-native";
+import { Icon } from "react-native-elements";
 
 class Chatbox extends Component {
   constructor(props) {
     super(props);
 
-    this.state ={
-      messages : [],
+    this.state = {
+      message: "",
     };
   }
 
-  handleOnChange = (event) =>{
+  handleOnChange = (event) => {
     this.setState({
-      messages: event.target.value
+      message: event.target.value,
     });
-  }
+  };
 
   handleSubmit = () => {
-
-  }
+    if (this.state.message === "") {
+      this.setState({ message: "" });
+    } else {
+      this.setState({ message: "" });
+    }
+  };
 
   render() {
     return (
-      <ScrollView>
-        <View>
-          <KeyboardAvoidingView style = {styles.container}>
-
+      <View style={styles.container}>
+        <View style={styles.bottomView}>
+          <KeyboardAvoidingView style={styles.footer}>
             <TextInput
-              value = {this.state.messages}
-              placeholder = "Type a message"
-              style = {styles.messages}
-              onChangeText = {this.handleOnChange}
+              value={this.state.messages}
+              placeholder="Type a message"
+              style={styles.messages}
+              onChangeText={this.handleOnChange}
+              returnKeyType="send"
             />
 
-            <Icon 
+            <Icon
               raised
               reverse
-              color = "#128c7e"
-              name = "paper-plane"
-              type = "font-awesome"
-              onPress = {this.handleSubmit}
+              size={20}
+              color="#128c7e"
+              style={{}}
+              name="paper-plane"
+              type="font-awesome"
+              onPress={this.handleSubmit}
             />
-
           </KeyboardAvoidingView>
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  messages: {
+    width: 300,
+    height: 45,
+    fontSize: 20,
+    backgroundColor: "#91c4ae",
+    borderRadius: 35,
+    paddingHorizontal: 25,
+    justifyContent: "flex-end",
+    position: "relative",
+  },
 
-messages: {
-  width: 300,
-  height: 45,
-  fontSize: 20,
-  backgroundColor: '#91c4ae',
-  borderRadius: 25,
-  paddingHorizontal: 25,
-  marginTop: 25,
-  justifyContent: "flex-end",
-  position: 'relative',
-},
-
-container: {
-  alignItems: 'center',
-  justifyContent: 'center',
-  flex: 1,
-  flexDirection: 'row',
-}
-
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "ios" ? 20 : 0,
+  },
+  bottomView: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+  },
+  footer: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+  },
+});
 
 export default Chatbox;
