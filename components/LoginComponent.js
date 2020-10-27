a5ce96f22a993ce6697124c55991668d6e16bfce
itsachaljain <achaljain785@gmail.com>
2020-10-27 18:31:49 +0530

    Login Page

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,17 +1,112 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Icon, Input, CheckBox, Button } from "react-native-elements";
+import * as SecureStore from "expo-secure-store";
 
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: "",
+      password: "",
+      remember: false,
+    };
+  }
+
+  componentDidMount() {
+    SecureStore.getItemAsync("userinfo").then((userdata) => {
+      let userinfo = JSON.parse(userdata);
+      if (userinfo) {
+        this.setState({ username: userinfo.username });
+        this.setState({ password: userinfo.password });
+        this.setState({ remember: true });
+      }
+    });
+  }
+
+  handleLogin() {
+    console.log(JSON.stringify(this.state));
+    if (this.state.remember) {
+      SecureStore.setItemAsync(
+        "userinfo",
+        JSON.stringify({
+          username: this.state.username,
+          password: this.state.password,
+        })
+      ).catch((error) => console.log("Could not save user info", error));
+    } else {
+      SecureStore.deleteItemAsync("userinfo").catch((error) =>
+        console.log("Could not delete user info", error)
+      );
+    }
   }
 
   render() {
     return (
-      <View>
-        <Text>Hey</Text>
-      </View>
+      <ScrollView style={{ backgroundColor: "aquamarine" }}>
+        <View style={styles.container}>
+          <Input
+            placeholder="Username"
+            leftIcon={{ type: "font-awesome", name: "user-o" }}
+            onChangeText={(username) => this.setState({ username })}
+            value={this.state.username}
+            containerStyle={styles.formInput}
+          />
+          <Input
+            placeholder="Password"
+            leftIcon={{ type: "font-awesome", name: "key" }}
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+            containerStyle={styles.formInput}
+          />
+          <CheckBox
+            title="remember me"
+            center
+            checked={this.state.remember}
+            onPress={() => this.setState({ remember: !this.state.remember })}
+            containerStyle={styles.formCheckbox}
+          />
+          <View style={styles.formButton}>
+            <Button
+              onPress={() => this.props.navigation.navigate("The Chatbox")}
+              title="Login"
+              color="#128c7e"
+              icon={
+                <Icon
+                  name="sign-in"
+                  type="font-awesome"
+                  size={24}
+                  color="white"
+                />
+              }
+              buttonStyle={{
+                backgroundColor: "#128c7e",
+              }}
+            />
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    margin: 5,
+    marginLeft: 0,
+  },
+  formInput: {
+    margin: 10,
+  },
+  formCheckbox: {
+    margin: 30,
+    backgroundColor: null,
+    borderColor: "aquamarine",
+  },
+  formButton: {
+    margin: 40,
+  },
+});
+
 export default Login;
